c987db17dace71710f54b6df0e3341dce9aaed8b
Alexandra Tello <ale@jegerost.com>
2015-07-08 12:48:59 -0500

    NodeJS Static App

--- dist/server.js
+++ dist/server.js
@@ -1,12 +1,13 @@
- 
+
 var express = require('express');
+var logger = require('morgan');
 var app = express();
- 
+
 app.set('port', process.env.PORT || 3000);
- 
-app.use(express.logger('dev'));
+
+app.use(logger('dev'));
 app.use(express.compress());
-app.use(express.static(__dirname + '/dist'));////
- 
+app.use(express.static(__dirname + '/public'));////
+
 app.listen(app.get('port'), function() {
 });
