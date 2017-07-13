08859c5b6f418038f9fb4d98d8159d0783c63e3b
Ale Tello <ipchan@gmail.com>
2017-07-13 00:31:16 -0400

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -6,7 +6,8 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 
 app.use(logger('dev'));
-app.use(express.static(__dirname + '/'));////
+app.use(express.static(__dirname + '/'));
 
 app.listen(app.get('port'), function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
